79d21aa95ff2a2bc700b6ecf43cb03ef7ab40a32
Marcial Lincoln <marcial.snake@gmail.com>
2023-09-06 05:52:58 -0300

    Rename node name virtual_joystick -> VirtualJoystick

--- addons/virtual_joystick/scripts/joystick_plugin.gd
+++ addons/virtual_joystick/scripts/joystick_plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_custom_type("virtual_joystick", "Node2D", preload("res://addons/virtual_joystick/scripts/virtual_joystick.gd"), preload("res://addons/virtual_joystick/sprites/icon.png"))
+	add_custom_type("VirtualJoystick", "Node2D", preload("res://addons/virtual_joystick/scripts/virtual_joystick.gd"), preload("res://addons/virtual_joystick/sprites/icon.png"))
 
 func _exit_tree() -> void:
-	remove_custom_type("virtual_joystick")
+	remove_custom_type("VirtualJoystick")
